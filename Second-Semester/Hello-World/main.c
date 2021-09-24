16ce8472e83dd808a71848df45e17b018733236b
slynkodenis <slynkodenis@mail.ru>
2021-09-24 10:02:30 +0300

    Add Euler number program

--- Second-Semester/Hello-World/main.c
+++ Second-Semester/Hello-World/main.c
@@ -6,20 +6,6 @@ int main(int argc, char **argv) {
     int max_threads = 0;
     int i = 0;
 
-    /*
-    #pragma omp parallel private(max_threads, i)
-    {
-        max_threads = omp_get_max_threads();
-        #pragma omp for ordered
-        for (i = 0; i < max_threads; ++i) {
-            #pragma omp ordered
-            {
-                printf("Hello World from %d / %d!\n", omp_get_thread_num(), max_threads);
-            }
-        }
-    }
-    */
-
     #pragma omp parallel shared(i)
     while(1) {
         if (omp_get_thread_num() == omp_get_num_threads() - 1 - i) {
---
+++ Second-Semester/Series-Sum/main.c
@@ -0,0 +1,47 @@
+#include <assert.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static double calculate_factorial(int number) {
+    double res = 1.0;
+    for (int i = number; i > 1; --i) {
+        res *= i;
+    }
+
+    return res;
+}
+ 
+
+int main(int argc, char **argv) {
+    int number_of_terms = 0;
+    if (argc == 1) {
+        fprintf(stderr, "Number of terms wasn't provided; using default value 100\n");
+        number_of_terms = 100;
+    } else {
+        number_of_terms = atoi(argv[1]);
+        if (number_of_terms < 1) {
+            fprintf(stderr, "Value from stdin: %d < 1\n", number_of_terms);
+            number_of_terms = 100;
+        }
+    }
+    int i = 0;
+    double sum = 1.0;
+
+    double *factorials = (double *) calloc(number_of_terms, sizeof(double));
+    assert(factorials);
+    #pragma omp parallel for private(i) shared(factorials, number_of_terms) default(none)
+    for (i = 0; i < number_of_terms; ++i) {
+        factorials[i] = calculate_factorial(i + 1);
+    }
+
+    #pragma omp parallel for private(i) shared(number_of_terms, factorials) reduction(+: sum) default(none)
+    for (i = 0; i < number_of_terms; ++i) {
+        sum += 1.0 / factorials[i];
+    }
+
+    printf("Calculated number %.20f with %d iterations\n", sum, number_of_terms);
+    free(factorials);
+    return 0;
+}
